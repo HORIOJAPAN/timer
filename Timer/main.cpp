dc9dbedc9404ef209da78d77e9e527f98dfe33ff
KosukeNakamura <s15mh133@mail.saitama-u.ac.jp>
2015-09-25 23:54:30 +0900

    いろいろ変更

--- Timer/main.cpp
+++ Timer/main.cpp
@@ -7,25 +7,24 @@ void main()
 
 	Timer t;//Timerの宣言
 
-	t.Start(0);//測定開始点
-	t.Start(1);//2つ目の計測開始点
+	t.Start();//測定開始点
 
 	//文字列を作成して標準出力に流す
-	for (int i = 1; i <= 10000; i++)
+	for (int i = 1; i <= 100; i++)
 	{
 		string str("⊂二二二（ ＾ω＾）二⊃ ﾌﾞｰﾝ");
 		cout << str << endl;
 
 		//10回ごとに時間を保存して開始時間の初期化を行う
-		if (!(i % 1000))
+		if (!(i % 10))
 		{
-			t.End(1, "test.txt");
-			t.Start(1);
+			t.getLapTime();
 		}
 	}
 
-	t.End(0,"test.txt","処理全体", Timer::sec);//全体の処理時間の測定結果保存
+	t.Save("test.txt", Timer::millisec);//全体の処理時間の測定結果保存
 
+	cout << t.getNowTime() << endl;
 
 	z = getchar();
 
